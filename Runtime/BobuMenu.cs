4b3f53a88d7d088694d1cb22e1e69dd6778a7e8e
Bahri Öztürk <123960501+bahriozturk@users.noreply.github.com>
2023-11-17 11:54:44 +0300

    Add files via upload

--- Runtime/BobuMenu.cs
+++ Runtime/BobuMenu.cs
@@ -46,13 +46,13 @@ public class MenuManagers : BobuMenuBase
         GetWindow<MenuManagers>("Import Scripts");
     }
 
-    protected override string Owner => "ownerA";
-    protected override string Repo => "repoA";
+    protected override string Owner => "kidstellar";
+    protected override string Repo => "BobuScripts";
 
     private void OnGUI()
     {
         GUILayout.Label("Managers");
-        
+
         if (GUILayout.Button("Import GameManager"))
             ImportScript("GameManager.cs");
 
@@ -75,8 +75,8 @@ public class MenuControllers : BobuMenuBase
         GetWindow<MenuControllers>("Import Scripts");
     }
 
-    protected override string Owner => "ownerB";
-    protected override string Repo => "repoB";
+    protected override string Owner => "kidstellar";
+    protected override string Repo => "BobuScripts";
 
     private void OnGUI()
     {
@@ -95,8 +95,8 @@ public class MenuAnchor : BobuMenuBase
         GetWindow<MenuAnchor>("Import Scripts");
     }
 
-    protected override string Owner => "ownerB";
-    protected override string Repo => "repoB";
+    protected override string Owner => "kidstellar";
+    protected override string Repo => "BobuScripts";
 
     private void OnGUI()
     {
@@ -115,8 +115,8 @@ public class MenuTutorial : BobuMenuBase
         GetWindow<MenuTutorial>("Import Scripts");
     }
 
-    protected override string Owner => "ownerB";
-    protected override string Repo => "repoB";
+    protected override string Owner => "kidstellar";
+    protected override string Repo => "BobuScripts";
 
     private void OnGUI()
     {
@@ -126,151 +126,3 @@ public class MenuTutorial : BobuMenuBase
             ImportScript("TutorialAnimation.cs");
     }
 }
-
-
-//namespace BobuMenu
-//{
-//    public class BobuMenuBase : EditorWindow
-//    {
-//        [MenuItem("Window/BobuMenu/Scripts")]
-//        public static void ShowWindow()
-//        {
-//            GetWindow<BobuMenuBase>("Import Scripts");
-//        }
-
-
-
-//        private void OnGUI()
-//        {
-
-//            if (GUILayout.Button("Import GameManager"))
-//                ImportScript("GameManager.cs");
-
-//            if (GUILayout.Button("Import LevelManager"))
-//                ImportScript("LevelManager.cs");
-
-//            if (GUILayout.Button("Import ButtonManager"))
-//                ImportScript("ButtonManager.cs");
-
-//            if (GUILayout.Button("Import LevelController"))
-//                ImportScript("LevelController.cs");
-
-//            if (GUILayout.Button("Import AnchorGameObject"))
-//                ImportScript("AnchorGameObject.cs");
-
-//            if (GUILayout.Button("Import TutorialDragAnimation"))
-//                ImportScript("TutorialAnimation.cs");
-
-//            if (GUILayout.Button("Import Audio"))
-//                ImportScript("AudioManager.cs");
-//        }
-
-//        protected void ImportScript(string scriptName)
-//        {
-//            string githubApiUrl = "https://api.github.com/repos/{owner}/{repo}/contents/{path}";
-//            string owner = "kidstellar";
-//            string repo = "BobuScripts";
-//            string path = scriptName;
-
-//            string apiUrl = githubApiUrl.Replace("{owner}", owner).Replace("{repo}", repo).Replace("{path}", path);
-
-//            UnityWebRequest request = UnityWebRequest.Get(apiUrl);
-//            request.SetRequestHeader("Accept", "application/vnd.github.v3.raw");
-//            request.SetRequestHeader("User-Agent", "UnityWebRequest");
-
-//            request.SendWebRequest().completed += operation =>
-//            {
-//                if (request.result == UnityWebRequest.Result.Success)
-//                {
-//                    string scriptContent = request.downloadHandler.text;
-//                    string destinationPath = "Assets/Scripts/" + scriptName;
-//                    System.IO.File.WriteAllText(destinationPath, scriptContent);
-//                    AssetDatabase.Refresh();
-//                    Debug.Log("Script imported successfully!");
-//                }
-//                else
-//                {
-//                    Debug.LogError("Error importing script: " + request.error);
-//                }
-//            };
-//        }
-//    }
-//}
-
-//namespace BobuMenu
-//{
-//    public class Managers : BobuMenuBase
-//    {
-//        [MenuItem("Window/BobuMenu/Scripts/Managers")]
-
-//        private void OnGUI()
-//        {
-//            if (GUILayout.Button("Import GameManager"))
-//                ImportScript("GameManager.cs");
-
-//            if (GUILayout.Button("Import LevelManager"))
-//                ImportScript("LevelManager.cs");
-
-//            if (GUILayout.Button("Import ButtonManager"))
-//                ImportScript("ButtonManager.cs");
-
-//            if (GUILayout.Button("Import Audio"))
-//                ImportScript("AudioManager.cs");
-//        }
-//    }
-
-//}
-
-//namespace BobuMenu
-//{
-//    public class Controllers : BobuMenuBase
-//    {
-//        [MenuItem("Window/BobuMenu/Scripts/Controllers")]
-
-//        private void OnGUI()
-//        {
-
-//            if (GUILayout.Button("Import LevelController"))
-//                ImportScript("LevelController.cs");
-//        }
-//    }
-//}
-
-//namespace BobuMenu
-//{
-//    public class Anchor : BobuMenuBase
-//    {
-//        [MenuItem("Window/BobuMenu/Scripts/Anchor")]
-
-//        private void OnGUI()
-//        {
-
-//            if (GUILayout.Button("Import AnchorGameObject"))
-//                ImportScript("AnchorGameObject.cs");
-
-//            if (GUILayout.Button("Import TutorialDragAnimation"))
-//                ImportScript("TutorialAnimation.cs");
-
-//            if (GUILayout.Button("Import Audio"))
-//                ImportScript("AudioManager.cs");
-//        }
-//    }
-//}
-
-//namespace BobuMenu
-//{
-//    public class Tutorial : BobuMenuBase
-//    {
-//        [MenuItem("Window/BobuMenu/Scripts/Tutorial")]
-
-//        private void OnGUI()
-//        {
-
-//            if (GUILayout.Button("Import TutorialDragAnimation"))
-//                ImportScript("TutorialAnimation.cs");
-
-//            if (GUILayout.Button("Import Audio"))
-//                ImportScript("AudioManager.cs");
-//        }
-//    }
-//}
