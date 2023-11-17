794f95d43f58fd2a7f4ce018ab1ded6e957bca95
Bahri Öztürk <123960501+bahriozturk@users.noreply.github.com>
2023-11-17 10:44:17 +0300

    Add files via upload

--- Runtime/BobuMenu.cs
+++ Runtime/BobuMenu.cs
@@ -14,23 +14,34 @@ public class BobuMenu : EditorWindow
 
     private void OnGUI()
     {
-        if (GUILayout.Button("Import DragDrop"))
-        {
-            ImportScript("AudioManager.cs");
-        }
+        if (GUILayout.Button("Import GameManager"))
+            ImportScript("GameManager.cs");
+
+        if (GUILayout.Button("Import LevelManager"))
+            ImportScript("LevelManager.cs");
+
+        if (GUILayout.Button("Import ButtonManager"))
+            ImportScript("ButtonManager.cs");
+
+        if (GUILayout.Button("Import LevelController"))
+            ImportScript("LevelController.cs");
+
+        if (GUILayout.Button("Import AnchorGameObject"))
+            ImportScript("AnchorGameObject.cs");
+
+        if (GUILayout.Button("Import TutorialDragAnimation"))
+            ImportScript("TutorialAnimation.cs");
 
         if (GUILayout.Button("Import Audio"))
-        {
             ImportScript("AudioManager.cs");
-        }
     }
 
     private void ImportScript(string scriptName)
     {
         string githubApiUrl = "https://api.github.com/repos/{owner}/{repo}/contents/{path}";
-        string owner = "kidstellar"; // GitHub kullan?c? ad? veya organizasyon ad?
-        string repo = "BobuScripts"; // GitHub reposu ad?
-        string path = scriptName; // GitHub'daki scriptin yolunu i?eren dosya yolu
+        string owner = "kidstellar";
+        string repo = "BobuScripts";
+        string path = scriptName;
 
         string apiUrl = githubApiUrl.Replace("{owner}", owner).Replace("{repo}", repo).Replace("{path}", path);
 
@@ -43,7 +54,7 @@ public class BobuMenu : EditorWindow
             if (request.result == UnityWebRequest.Result.Success)
             {
                 string scriptContent = request.downloadHandler.text;
-                string destinationPath = "Assets/Scripts/" + scriptName; // Scriptin projeye kopyalanaca?? yol
+                string destinationPath = "Assets/Scripts/" + scriptName;
                 System.IO.File.WriteAllText(destinationPath, scriptContent);
                 AssetDatabase.Refresh();
                 Debug.Log("Script imported successfully!");
